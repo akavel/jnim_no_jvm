7978644d848a069950d24069e4f42fcd1dba2ccf
Mateusz Czapliński <czapkofan@gmail.com>
2019-09-22 20:18:40 +0200

    failure even if just trying to dump the AST

--- src/jnim_no_jvm.nim
+++ src/jnim_no_jvm.nim
@@ -1,7 +1,9 @@
 import jnim
+import macros
 
-proc Java_com_akavel_hello2_HelloActivity_stringFromJNI*(env: JNIEnvPtr, thiz: jobject): jstring {.cdecl,exportc,dynlib.} =
-  return env.NewStringUTF(env, "Hello from Nim :D")
+dumpTree:
+  proc Java_com_akavel_hello2_HelloActivity_stringFromJNI*(env: JNIEnvPtr, thiz: jobject): jstring {.cdecl,exportc,dynlib.} =
+    return env.NewStringUTF(env, "Hello from Nim :D")
 
 when isMainModule:
   echo("Hello, World!")
